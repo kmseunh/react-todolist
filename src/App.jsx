d144dd306354719cebdb0104d8d13c50fab6848d
kmseunh <tmdgus8779@gmail.com>
2023-08-24 00:51:51 +0900

    feat: Add structured layout using styled components
    Created a structured layout for the React app using styled-components
    The AppContainer component employs CSS grid to organize the header, main content, and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { styled } from 'styled-components';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
+const AppContainer = styled.div`
+    display: grid;
+    grid-template-rows: auto 13fr 1fr;
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    return (
+        <>
+            <AppContainer>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+                <Footer />
+            </AppContainer>
+        </>
+    );
+};
 
-export default App
+export default App;
