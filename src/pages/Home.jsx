cb5d6a5128f01708a258e84c31a28066946be1b1
kmseunh <tmdgus8779@gmail.com>
2023-08-25 00:21:16 +0900

    refactor: edit Home component with styled container
    Revise 'Home' component with styled container using 'styled-components'
    Enhance 'addTodo' for better data management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+import Input from '../components/Input';
+import TodoList from '../components/TodoList';
+
+const HomeContainer = styled.div`
+    background-color: #f6f6f6;
+    width: 110vh;
+    height: 30vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+`;
+
 const Home = () => {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (newTodo) => {
+        setTodos([...todos, newTodo]);
+    };
+
     return (
         <div>
-            <p>Hello, Todo!</p>
+            <HomeContainer>
+                <h1>Todo List</h1>
+                <Input onAddTodo={addTodo} />
+                <div>
+                    <TodoList todos={todos} />
+                </div>
+            </HomeContainer>
         </div>
     );
 };
