e368bec85a8669c7cd3d86ab7451f54b41563506
kmseunh <tmdgus8779@gmail.com>
2023-08-25 00:16:58 +0900

    feat: create a Input.jsx
    Develop styled 'Input' component using 'styled-components'
    Enables adding todos with validation

---
+++ src/components/Input.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+import { styled } from 'styled-components';
+import Button from './Button';
+
+const StyledInput = styled.input`
+    width: 50rem;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.2);
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    box-sizing: border-box;
+    margin-right: 10px;
+`;
+
+const Input = ({ onAddTodo }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleClick = () => {
+        if (inputValue.trim() !== '') {
+            const newTodo = {
+                id: Date.now(),
+                text: inputValue,
+            };
+            onAddTodo(newTodo);
+            setInputValue('');
+        } else {
+            alert('Todo를 입력해주세요!!');
+        }
+    };
+
+    return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <StyledInput
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <Button onClick={handleClick}>Add</Button>
+        </div>
+    );
+};
+
+export default Input;
