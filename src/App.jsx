320fb559f9a13fa7782ed62e2cf56da7d0b44cd6
kmseunh <tmdgus8779@gmail.com>
2023-08-25 00:22:38 +0900

    refactor: update layout using styled components
    Revise 'App' component layout with flex using styled components for centered content. Maintain routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import { styled } from 'styled-components';
 import Home from './pages/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 const AppContainer = styled.div`
-    display: grid;
-    grid-template-rows: auto 13fr 1fr;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 `;
 
 const App = () => {
     return (
         <>
             <AppContainer>
-                <Header />
                 <Routes>
                     <Route path='/' element={<Home />} />
                 </Routes>
-                <Footer />
             </AppContainer>
         </>
     );
