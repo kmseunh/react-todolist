becf2e9b2df92b300c31b0754620883167827a01
kmseunh <tmdgus8779@gmail.com>
2024-01-09 17:16:49 +0900

    refactor: refactor code structure by integrating useContext with existing useReducer implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-import { useReducer } from 'react';
 import AddTask from './components/AddTasks.jsx';
 import TaskList from './components/TaskList.jsx';
-import tasksReducer from './reducers/taskReducer.js';
+import { TasksProvider } from './contexts/TasksContext.jsx';
 
 export default function TaskApp() {
-    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-
-    function handleAddTask(text) {
-        dispatch({
-            type: 'added',
-            id: nextId++,
-            text: text,
-        });
-    }
-
-    function handleChangeTask(task) {
-        dispatch({
-            type: 'changed',
-            task: task,
-        });
-    }
-
-    function handleDeleteTask(taskId) {
-        dispatch({
-            type: 'deleted',
-            id: taskId,
-        });
-    }
-
     return (
-        <>
-            <h1>Prague itinerary</h1>
-            <AddTask onAddTask={handleAddTask} />
-            <TaskList
-                tasks={tasks}
-                onChangeTask={handleChangeTask}
-                onDeleteTask={handleDeleteTask}
-            />
-        </>
+        <TasksProvider>
+            <h1>Day off in Kyoto</h1>
+            <AddTask />
+            <TaskList />
+        </TasksProvider>
     );
 }
-
-let nextId = 3;
-const initialTasks = [
-    { id: 0, text: 'Visit Kafka Museum', done: true },
-    { id: 1, text: 'Watch a puppet show', done: false },
-    { id: 2, text: 'Lennon Wall pic', done: false },
-];
